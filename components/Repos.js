af48d4c9627dc83a32d3937da91b718d064c0b15
Ruan Santiago <ruansanvasconcelos@gmail.com>
2021-07-01 13:48:20 -0300

    Adding Chart

--- components/Repos.js
+++ components/Repos.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const  {repos} = useContext(GithubContext)
+  console.log(repos)
+
+  const chartData = [
+    {
+      label: "Venezuela",
+      value: "290"
+    },
+    {
+      label: "Saudi",
+      value: "260"
+    },
+    {
+      label: "Canada",
+      value: "180"
+    }
+    
+  ];
+    
+
+  return( 
+  <section className="section">
+    <Wrapper className ="section-center">
+  <ExampleChart data = {chartData}/>
+  </Wrapper>
+  </section>
+  )
 };
 
 const Wrapper = styled.div`
